8e8299f186a5bdf985b828588040b0cb5a885621
wupeng <1421386897@qq.com>
2026-02-26 17:16:41 +0800

    feat：新增环境config配置

---
+++ utils/config.js
@@ -0,0 +1,16 @@
+const config = {
+  development: {
+    baseUrl: '/api',
+    baseUrlActual: 'http://192.168.1.24:8080',
+    env: 'development'
+  },
+  production: {
+    baseUrl: '/api',
+    baseUrlActual: 'http://sys.ruizhongzx.com',
+    env: 'production'
+  }
+}
+
+// 根据环境获取配置
+const env = process.env.NODE_ENV === 'development' ? 'development' : 'production'
+export default config[env]
